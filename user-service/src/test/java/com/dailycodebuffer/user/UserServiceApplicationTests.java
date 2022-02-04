982566b38318e578a79e4be4a14ec333758ff5b7
narendracharugundla <naren.kumar426@gmail.com>
2022-02-04 10:20:45 +0530

    testing merge request

--- user-service/src/test/java/com/dailycodebuffer/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/dailycodebuffer/user/UserServiceApplicationTests.java
@@ -3,6 +3,7 @@ package com.dailycodebuffer.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//Testing
 @SpringBootTest
 class UserServiceApplicationTests {
 
